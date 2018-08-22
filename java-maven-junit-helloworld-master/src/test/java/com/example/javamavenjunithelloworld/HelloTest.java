1fb4728283904873e2cc752488353c053adf4ef3
Nidhin Nair R U <39849454+nidhinnru@users.noreply.github.com>
2018-08-22 05:12:10 -0500

    Update HelloTest.java

--- java-maven-junit-helloworld-master/src/test/java/com/example/javamavenjunithelloworld/HelloTest.java
+++ java-maven-junit-helloworld-master/src/test/java/com/example/javamavenjunithelloworld/HelloTest.java
@@ -38,7 +38,9 @@ public class HelloTest {
         hi.sayHello(stream);
 
         // Does it say "Hello!" three times?
-        String goal = Hello.HELLO + "\r\n" + Hello.HELLO + "\r\n" + Hello.HELLO + "\r\n";
+        //String goal = Hello.HELLO + "\r\n" + Hello.HELLO + "\r\n" + Hello.HELLO + "\r\n";
+        String goal = Hello.HELLO + "\n" + Hello.HELLO + "\n" + Hello.HELLO + "\n";
+
         assertThat(os.toString(), is(equalTo(goal)));
     }
 
